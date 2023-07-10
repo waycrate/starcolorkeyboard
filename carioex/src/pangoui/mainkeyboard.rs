7ff58bb561ee32165ce19dc9594bd68e75e7bf81
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-07-10 11:06:45 +0800

    chore: add keys

--- carioex/src/pangoui/mainkeyboard.rs
+++ carioex/src/pangoui/mainkeyboard.rs
@@ -6,7 +6,7 @@ struct DrawInfo<'a> {
     font_size: i32,
     line: i32,
     text: &'a str,
-    start_x: f64,
+    start_pos: i32,
 }
 
 fn draw_unit_key(
@@ -18,9 +18,10 @@ fn draw_unit_key(
         font_size,
         line,
         text,
-        start_x,
+        start_pos,
     }: DrawInfo,
 ) {
+    let start_x = step * start_pos as f64;
     let end_x = step * width as f64 + start_x;
     let start_y = step * line as f64;
     let end_y = step * (line + 1) as f64;
@@ -60,7 +61,127 @@ pub fn draw_main_keyboard(
             font_size,
             line: 0,
             text: "Tab",
-            start_x: 0.0,
+            start_pos: 0,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "q",
+            start_pos: 2,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "w",
+            start_pos: 3,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "e",
+            start_pos: 4,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "r",
+            start_pos: 5,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "t",
+            start_pos: 6,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "y",
+            start_pos: 7,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "u",
+            start_pos: 8,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "i",
+            start_pos: 9,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "o",
+            start_pos: 10,
+        },
+    );
+    draw_unit_key(
+        pangolayout,
+        content,
+        DrawInfo {
+            step: step as f64,
+            width: 1,
+            font_size,
+            line: 0,
+            text: "p",
+            start_pos: 11,
         },
     );
 }
